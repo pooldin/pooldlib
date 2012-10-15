7a2a72e298b1e3b72aa387d0ce0cd3ed3bb13483
Brian Oldfield <brian.oldfield@gmail.com>
2012-10-15 16:57:02 -0700

    Added pooldlib.sqlalchemy.session_transaction context manager for handling atomic transactions.

--- pooldlib/sqlalchemy/__init__.py
+++ pooldlib/sqlalchemy/__init__.py
@@ -1,1 +1,2 @@
 from .database import Database
+from .transaction import transaction_session
---
+++ pooldlib/sqlalchemy/transaction.py
@@ -0,0 +1,33 @@
+"""
+pooldlib.sqlalchemy.transaction
+===============================
+
+.. currentmodule:: pooldlib.sqlalchemy.transaction
+
+"""
+from contextlib import contextmanager
+
+
+@contextmanager
+def transaction_session(session=None, auto_commit=False):
+    """Context manager to facilitate atomic transactions. If any exception occurs
+    over the course of the contained transaction, ``session.rollback()`` will
+    be called.
+
+    :param session: Session in which to execute transaction.
+    :type session: :class:`pooldlib.postgresql.db.session` instance or `None`.
+    :param auto_commit: Whether or not to call ``session.commit()``
+                        automatically when the context exits.
+    :type auto_commit: boolean, defaults to `False`.
+    """
+    from pooldlib.postgresql import db
+    session = session or db.session
+    try:
+        yield session
+    # Catch any and every exception and execute a rollback
+    except Exception, e:
+        session.rollback()
+        # Now re-raise the exception to let it continue to buble up
+        raise e
+    if auto_commit:
+        session.commit()
