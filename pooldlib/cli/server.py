00b14b373ed44f2c19e41eb64e72ad3511b46c1e
Collin Watson <collinwat@gmail.com>
2012-10-17 13:03:50 -0700

    Added debugger and reload options to the cli server command

--- pooldlib/cli/server.py
+++ pooldlib/cli/server.py
@@ -8,14 +8,24 @@ class ServerController(controller.CementBaseController):
     class Meta:
         label = 'runserver'
         description = "Run the flask development server"
+        arguments = [
+            (['-d', '--debugger'], {
+                'action': 'store_true',
+                'help': 'Run the server with the debugger enabled'
+            }),
+            (['-r', '--reload'], {
+                'action': 'store_true',
+                'help': 'Run the server with the reloader enabled'
+            }),
+        ]
 
     @controller.expose(hide=True, help='Run the flask development server')
     def default(self):
-        app = getattr(self, 'flask_app')
-        self.run(app)
+        self.run(getattr(self, 'flask_app'),
+                 use_debugger=self.pargs.debugger,
+                 use_reloader=self.pargs.reload)
 
     def run(self, app, **kw):
-        kw['debug'] = True
         kw.setdefault('host', os.environ.get('HOST', '0.0.0.0'))
         kw.setdefault('port', int(os.environ.get('PORT', 5000)))
 
