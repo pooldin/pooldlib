e9b5b1759abe03eff8b66b2a9733c6d6d7786413
Brian Oldfield <brian.oldfield@gmail.com>
2012-10-19 14:11:34 -0700

    Made  values in KeyValueMixin db indexes.

--- pooldlib/postgresql/common/keyvalue.py
+++ pooldlib/postgresql/common/keyvalue.py
@@ -2,5 +2,5 @@ from pooldlib.postgresql import db
 
 
 class KeyValueMixin(object):
-    key = db.Column(db.String(64), nullable=False)
+    key = db.Column(db.String(64), nullable=False, index=True)
     value = db.Column(db.Text, nullable=False)
