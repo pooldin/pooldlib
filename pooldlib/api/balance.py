51714d75f10b0e1e8488441f2f514ac4ccb7a19c
Brian Oldfield <brian.oldfield@gmail.com>
2012-10-25 18:32:29 -0700

    Added balance.create_for_community and balance.create_for_user

--- pooldlib/api/balance.py
+++ pooldlib/api/balance.py
@@ -5,8 +5,11 @@ pooldlib.api.balance
 .. currentmodule:: pooldlib.api.balance
 
 """
+from decimal import Decimal
+
 from sqlalchemy.orm.attributes import manager_of_class
 
+from pooldlib.sqlalchemy import transaction_session
 from pooldlib.postgresql import Balance as BalanceModel
 
 
@@ -17,9 +20,6 @@ def get(for_update=False, **kwargs):
     """Retrieve :class:`pooldlib.postgresql.models.Balance` objects from the
     database. If ``for_update`` is `True` it is assumed that the caller wants a
     single balance object retrieved.
-
-    TODO :: It would be much more efficient to retrieve all
-            `pooldlib.postgresql.models.Balance` objects to be updated at once.
 
     :param for_update: If `True` the ``FOR UPDATE`` directive will be used, locking the row for an ``UPDATE`` query.
     :type for_update: boolean, default `False`
@@ -39,3 +39,31 @@ def get(for_update=False, **kwargs):
     else:
         balance = q.all()
     return balance
+
+
+def create_for_community(community, currency):
+    b = BalanceModel()
+    b.enabled = True
+    b.amount = Decimal('0.0000')
+    b.currency = currency
+    b.community = community
+    b.type = 'community'
+
+    with transaction_session() as session:
+        session.add(b)
+        session.commit()
+    return b
+
+
+def create_for_user(user, currency):
+    b = BalanceModel()
+    b.enabled = True
+    b.amount = Decimal('0.0000')
+    b.currency = currency
+    b.user = user
+    b.type = 'user'
+
+    with transaction_session() as session:
+        session.add(b)
+        session.commit()
+    return b
