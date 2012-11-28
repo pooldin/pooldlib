89403f0c70c0c4139f1533bc99b9537067885f1a
Brian Oldfield <brian.oldfield@gmail.com>
2012-11-28 12:14:16 -0800

    Removed user fk from Fee model.

--- pooldlib/postgresql/models/fee.py
+++ pooldlib/postgresql/models/fee.py
@@ -10,10 +10,6 @@ class Fee(common.ConfigurationModel):
     flat = db.Column(db.DECIMAL(precision=8, scale=4),
                      nullable=False,
                      default=0)
-    user_id = db.Column(db.BigInteger(unsigned=True),
-                        db.ForeignKey('user.id'),
-                        nullable=False)
-    user = db.relationship('User', backref='associated_fees', lazy='select')
 
     @property
     def percentage(self):
